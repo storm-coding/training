2af212b4669ace7da670d8df4efeb29ac5304ceb
storm-coding <15900519717@163.com>
2019-01-08 23:06:35 +0800

    添加 leetcode003解题方法  新增 leetcode011

--- src/main/java/com/hjn/leetcode/LongestSubstring.java
+++ src/main/java/com/hjn/leetcode/LongestSubstring.java
@@ -1,11 +1,22 @@
 package com.hjn.leetcode;
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
- * leetcode 003
+ * LeetCode 003
  * 3. Longest Substring Without Repeating Characters
  * Given a string, find the length of the longest substring without repeating characters.
  */
 public class LongestSubstring {
+    /**
+     *     最长字串，解决方法：在left和right之间的表示是不重复的。时间复杂度O(1)。leetcode时间34 ms
+     *     使用数组表示ascii对应的字符
+     */
     public int lengthOfLongestSubstring(String s) {
         int left = 0;
         int right = -1;
@@ -27,5 +38,28 @@ public class LongestSubstring {
             }
         }
         return result;
+    }
+
+    /**
+     * 思路和上面一样。不重复的元素使用hashSet保存。leetcode时间51 ms
+     */
+    public static int lengthOfLongestSubstring1(String s) {
+        Set<Character> charNum = new HashSet<>();
+        int left = 0;
+        int right = -1;
+        int result = 0;
+        while (left < s.length()) {
+            if (right < s.length() -1 && !charNum.contains(s.charAt(right + 1))) {
+                charNum.add(s.charAt(++right));
+                result = Math.max(result, charNum.size());
+            } else {
+                charNum.remove(s.charAt(left++));
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring1("abcabcbb"));
     }
 }
---
+++ src/main/java/com/hjn/leetcode/MostWater.java
@@ -0,0 +1,30 @@
+package com.hjn.leetcode;
+
+/**
+ * LeetCode 011
+ * Given n non-negative integers a1, a2, ..., an ,where each represents a point at coordinate (i, ai).
+ * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+ * Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+ Note: You may not slant the container and n is at least 2.
+ */
+public class MostWater {
+    /**
+     * 题目的意思就是，给出一个数组。找出在数轴上的最大面积。
+     * 思路：分别从数组的左右起点，相向靠近
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int result = 0;
+        while (left < right){
+            int temp = (right-left) * (height[left] - height[right] > 0 ? height[left++] : height[right--]);
+            if(temp > result) {
+                result = temp;
+            }
+        }
+        return result;
+    }
+}
