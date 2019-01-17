7ce8a799bf8ecbbaeb28e219d0ac82a0f67ea7e6
storm-coding <15900519717@163.com>
2019-01-17 23:29:56 +0800

    添加 Leetcode 283

--- src/main/java/com/hjn/leetcode/MoveZeroes.java
+++ src/main/java/com/hjn/leetcode/MoveZeroes.java
@@ -1,7 +1,23 @@
 package com.hjn.leetcode;
 
 /**
- * Created by xiaoz on 2019/1/16.
+ * Leetcode283
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
  */
 public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int index = -1;
+
+        for(int i=0;i<nums.length;i++){
+
+            if(nums[i]!=0){
+                index++;
+                if(index!=i) {
+                    int temp = nums[index];
+                    nums[index] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+        }
+    }
 }
