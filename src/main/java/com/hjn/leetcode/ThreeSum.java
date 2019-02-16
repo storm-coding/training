272a66d9aeced111d6ce1dd8bc813c043ba691ec
storm-coding <15900519717@163.com>
2019-02-16 21:52:17 +0800

    更新 leetcode015

--- src/main/java/com/hjn/leetcode/ThreeSum.java
+++ src/main/java/com/hjn/leetcode/ThreeSum.java
@@ -1,8 +1,6 @@
 package com.hjn.leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * leetcode015
@@ -12,10 +10,10 @@ import java.util.List;
 public class ThreeSum {
     /**
      * 题目意思： 给出一个int数组，是不是存在元素a、b、c使得a+b+c=0，
-     *            找出所有这样独一无二的组合使得他们的和为0
+     * 找出所有这样独一无二的组合使得他们的和为0
      * 思路： 1、先对数组进行排序
-     *        2、遍历数组，对当前的节点，生于两个节点使用2Sum的思路进行查找
-     *        3、对符合节点的集合进行去重
+     * 2、遍历数组，对当前的节点，生于两个节点使用2Sum的思路进行查找
+     * 3、对符合节点的集合进行去重
      */
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
@@ -31,14 +29,14 @@ public class ThreeSum {
                         boolean find = false;
                         for (List<Integer> list : result) {
 
-                             if ((list.get(0) == nums[i] && list.get(1) == nums[left] && list.get(2) == nums[right]) ||
+                            if ((list.get(0) == nums[i] && list.get(1) == nums[left] && list.get(2) == nums[right]) ||
                                     (list.get(0) == nums[i] && list.get(1) == nums[right] && list.get(2) == nums[left]) ||
                                     (list.get(0) == nums[left] && list.get(1) == nums[i] && list.get(2) == nums[right]) ||
                                     (list.get(0) == nums[left] && list.get(1) == nums[right] && list.get(2) == nums[i]) ||
                                     (list.get(0) == nums[right] && list.get(1) == nums[left] && list.get(2) == nums[i]) ||
                                     (list.get(0) == nums[right] && list.get(1) == nums[i] && list.get(2) == nums[left])
                                     ) {
-                                 find = true;
+                                find = true;
                                 break;
                             }
                         }
@@ -71,10 +69,63 @@ public class ThreeSum {
         return result;
     }
 
+    /**
+     * 思路2: 思路和上面一样，优化结果集的去重。
+     *        主要是对每个位置使用set进行去重，因为每个位置只需要在遍历中出现一次
+     */
+    public List<List<Integer>> threeSum1(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length < 3) {
+            return result;
+        }
+        Arrays.sort(nums);
+        Set<Integer> hashCount = new HashSet();
+        for (int i = 0; i < nums.length; i++) {
+            if(hashCount.contains(nums[i])) {
+                continue;
+            }
+            hashCount.add(nums[i]);
+            int left = i + 1, right = nums.length - 1;
+
+            Set<Integer> sl = new HashSet<>();
+            Set<Integer> sr = new HashSet<>();
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] == 0) {
+                    if (sl.contains(nums[left])) {
+                        left ++;
+                        continue;
+                    }
+                    if (sl.contains(nums[right])) {
+                        right --;
+                        continue;
+                    }
+                    sl.add(nums[left]);
+                    sr.add(nums[right]);
+
+                    List<Integer> l = new ArrayList();
+                    l.add(nums[i]);
+                    l.add(nums[left]);
+                    l.add(nums[right]);
+                    result.add(l);
+                    left++;
+                    right--;
+                } else {
+                    if (nums[i] + nums[left] + nums[right] > 0) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         ThreeSum ts = new ThreeSum();
-        int[] nums = {-1, 0, 1, 2, -1, -4};
-        List<List<Integer>> l = ts.threeSum(nums);
+        int[] nums = {3,0,-2,-1,1,2};
+        List<List<Integer>> l = ts.threeSum1(nums);
         System.out.println(l.size());
     }
 }
