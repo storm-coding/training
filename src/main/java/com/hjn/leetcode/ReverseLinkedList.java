3ff7f4e1f6b83337876884d9b665926cfdeb5307
storm-coding <15900519717@163.com>
2019-02-23 21:57:00 +0800

    添加 leetcode 206

--- src/main/java/com/hjn/leetcode/ReverseLinkedList.java
+++ src/main/java/com/hjn/leetcode/ReverseLinkedList.java
@@ -1,7 +1,55 @@
 package com.hjn.leetcode;
 
+import com.hjn.leetcode.utils.LinkedUtils;
+import com.hjn.leetcode.utils.ListNode;
+
 /**
- * Created by xiaoz on 2019/2/22.
+ * leetcode 206
+ * Reverse a singly linked list.
  */
 public class ReverseLinkedList {
+    /**
+     * 题目意思： 反转一个链表
+     * 思路：循环解法
+     */
+    public ListNode reverseList(ListNode head) {
+
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
+
+    /**
+     * 思路： 递归解法
+     */
+    public ListNode reverseList1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode cur = head;
+        ListNode next = head.next;
+        head.next = null;
+        ListNode nNode = reverseList1(next);
+        next.next = cur;
+        return nNode;
+
+    }
+
+    public static void main(String[] args) {
+        ReverseLinkedList reverse = new ReverseLinkedList();
+        ListNode listNode = LinkedUtils.create(new int[]{1, 2, 3, 4, 5});
+        LinkedUtils.print(listNode);
+        listNode = reverse.reverseList1(listNode);
+        System.out.println("=======reverse========");
+        LinkedUtils.print(listNode);
+    }
+
+
 }
